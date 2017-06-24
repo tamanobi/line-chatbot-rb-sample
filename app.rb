3fc7fe21bf864a0e79832993f2ffed103280dc20
tomitomi0830 <tomimasa0830@gmail.com>
2017-06-25 00:15:38 +0900

    Update app.rb

--- app.rb
+++ app.rb
@@ -60,7 +60,7 @@ post '/callback' do
       when Line::Bot::Event::MessageType::Text
         message = {
           type: 'text',
-          text: event.message['text']
+          text: 'text'
         }
         client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
