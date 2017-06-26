f6609c1529cfb8180f0e6be8ec2863ed23a6e2ff
tomitomi0830 <tomimasa0830@gmail.com>
2017-06-26 21:35:56 +0900

    Update app.rb

--- app.rb
+++ app.rb
@@ -88,26 +88,30 @@ post '/callback' do
                 text: 'なんのごようですか？'
             }
         client.reply_message(event['replyToken'], message)
-      when response = 'スニーカーが欲しいな'
-        message = {
-            type: 'template',
-            thumbnailImageUrl: 'https://github.com/tomitomi0830/line-chatbot-rb-sample/blob/master/img1.jpg',
-            title: 'Menu',
-            text: 'Please select',
-            actions: [
-              {
-                type: 'postback',
-                label: 'Buy',
-                data:  'action=buy&itemid=123'
-              },
-              {
-                type: 'postback',
-                label: 'Add to cart',
-                data:  'action=add&itemid=123'                      
+        when response = 'スニーカーが欲しいな'
+          message = {
+              type: 'template',
+              altText: 'this is a buttons template',
+              template: {
+                type: 'buttons'
+                thumbnailImageUrl: 'https://github.com/tomitomi0830/line-chatbot-rb-sample/blob/master/img1.jpg',
+                title: 'Menu',
+                text: 'Please select',
+                actions: [
+                  {
+                    type: 'postback',
+                    label: 'Buy',
+                    data:  'action=buy&itemid=123'
+                  },
+                  {
+                    type: 'postback',
+                    label: 'Add to cart',
+                    data:  'action=add&itemid=123'                      
+                  }
+                ]
               }
-            ]
-          }
-      client.reply_message(event['replyToken'], message)           
+            }            
+        client.reply_message(event['replyToken'], message)           
         else
             message = {
                 type: 'text',
